a2bdb0528bd217041de4028a8618fa00f5f9789a
Mandy Arens <40013860+maanderz@users.noreply.github.com>
2020-04-01 10:38:07 -0600

    can get data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [data, setTotalReactPackages] = useState("");
+
+  useEffect(() => {
+    // GET request using fetch inside useEffect React hook
+    fetch("https://api.npms.io/v2/search?q=react")
+      .then(response => response.json())
+      .then(data => setTotalReactPackages(data.total));
+    // empty dependency array means this effect will only run once (like componentDidMount in classes)
+  }, []);
+
+  console.log("data", data);
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
