11ed8c602b10a61000b9c5e7d193e10c616854f0
Mandy Arens <40013860+maanderz@users.noreply.github.com>
2020-04-03 13:49:56 -0600

    added in footer with content

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./Footer.css";
 import Badge from "../Images/edamam.png";
+import ReactBadge from "../Images/react.png";
 
 function Footer() {
   return (
-    <footer className="test1">
-      <div className="badge">
+    <footer className="footer">
+      <div className="footer-author">
+        {" "}
+        <a href="https://github.com/maanderz">Created by: Mandy A</a>
+      </div>
+      <div className="edamam-badge">
         <a href="https://developer.edamam.com/">
           <img src={Badge} alt="badge" />
+        </a>
+      </div>
+      <div className="react-badge">
+        <a href="https://reactjs.org/">
+          <span> ReactJS </span> <img src={ReactBadge} alt="badge" />
         </a>
       </div>
     </footer>
