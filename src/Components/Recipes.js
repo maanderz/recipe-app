86e5f143aafccd517bfed1b261f29b5b6d5f50e8
Mandy Arens <40013860+maanderz@users.noreply.github.com>
2020-04-04 16:13:47 -0600

    map query into component

--- src/Components/Recipes.js
+++ src/Components/Recipes.js
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Recipes.css";
 
 function Recipe(props) {
-  console.log("from recipes", props.query);
+  let query = props.query.replace(/\s/g, "+");
+  const [card, setCard] = useState([]);
 
-  return <div className="test">Stuff</div>;
+  console.log(query);
+
+  useEffect(query => {
+    // GET request using fetch inside useEffect React hook
+    fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${
+        process.env.app_id
+      }&app_key=${process.env.api_key}`
+    )
+      .then(response => response.json())
+      .then(data => setCard(data));
+    // empty dependency array means this effect will only run once (like componentDidMount in classes)
+  }, []);
+
+  console.log(card);
+
+  return (
+    <div className="recipes-section">
+      {card.hits.map((recipe, index) => {
+        return (
+          <div key={index} className="card" style={{ width: 250 }}>
+            <img
+              className="card-img-top"
+              src={recipe.recipe.image}
+              alt="recipe"
+            />
+            <div className="card-body">
+              <a href={recipe.recipe.uri}>
+                <h5 className="card-title">{recipe.recipe.label}</h5>
+              </a>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Recipe;
